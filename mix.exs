2efe89c5d69d44d5a02ce51a683e9496ae189e50
Ian Harris <ian@fofgof.xyz>
2024-06-28 08:37:09 -0700

    add erlang apps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Breakout.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :wx, :observer, :runtime_tools, :tools],
       mod: {Breakout.Application, []}
     ]
   end
