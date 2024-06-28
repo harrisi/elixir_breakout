683f785bf9b346611d1b17ad89e28bafbe665c1a
Ian Harris <ian@fofgof.xyz>
2024-06-28 08:35:54 -0700

    init commit

---
+++ lib/breakout.ex
@@ -0,0 +1,18 @@
+defmodule Breakout do
+  @moduledoc """
+  Documentation for `Breakout`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Breakout.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/breakout_test.exs
@@ -0,0 +1,8 @@
+defmodule BreakoutTest do
+  use ExUnit.Case
+  doctest Breakout
+
+  test "greets the world" do
+    assert Breakout.hello() == :world
+  end
+end
