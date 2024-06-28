7c340e79ad558f52a029927a587d19bbc7aa2cb2
Ian Harris <ian@fofgof.xyz>
2024-06-28 08:38:40 -0700

    remove generated test and `hello/0`

--- lib/breakout.ex
+++ lib/breakout.ex
@@ -2,17 +2,4 @@ defmodule Breakout do
   @moduledoc """
   Documentation for `Breakout`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Breakout.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/breakout_test.exs
+++ test/breakout_test.exs
@@ -2,7 +2,4 @@ defmodule BreakoutTest do
   use ExUnit.Case
   doctest Breakout
 
-  test "greets the world" do
-    assert Breakout.hello() == :world
-  end
 end
